ee531608797096bbed3c8a1f782b89ab18c36805
Vinícius Müller <vinigm.nho@gmail.com>
2023-02-02 17:46:20 -0300

    Remove attributes from generated components

--- assets/material_icons.exs
+++ assets/material_icons.exs
@@ -5,11 +5,11 @@ defmodule MaterialIcons do
 
   use Phoenix.Component
 
-  attr :rest, :global
-  attr :style, :string, required: true
-  attr :size, :integer, required: true
-  attr :paths, :map, required: true
+  attr :style, :string, values: ~w(filled outlined round sharp two-tone), default: "filled"
+  attr :size, :integer, default: 24
   slot :inner_block, required: true
+  attr :paths, :map, required: true
+  attr :class, :string, default: ""
   defp svg(assigns) do
     content = case assigns.paths[assigns.style] do 
       nil -> raise RuntimeError, "icon does not have the following style: #{assigns.style}"
@@ -19,7 +19,7 @@ defmodule MaterialIcons do
     assigns = assign(assigns, :content, content)
 
    ~H"""
-   <svg xmlns="http://www.w3.org/2000/svg" width={@size} height={@size} viewBox="0 0 24 24" {@rest}>
+   <svg xmlns="http://www.w3.org/2000/svg" class={@class} width={@size} height={@size} viewBox="0 0 24 24">
       <%%= @content %>
     </svg>
    """
@@ -30,9 +30,6 @@ defmodule MaterialIcons do
     @doc """
     Renders the `<%= func %>` icon.
     """
-    attr :rest, :global, doc: "the arbitrary HTML attributes for the svg container", include: ~w(fill stroke stroke-width)
-    attr :style, :string, values: ~w(filled outlined round sharp two-tone), default: "filled"
-    attr :size, :integer, default: 24
     def <%= func %>(assigns), do: svg(assign(assigns, paths: <%= Macro.to_string(paths) %>))
   <% end %>
 end
