54d4b5463160fe9f5e06fc5dfc060b7fe5831d1f
Vinícius Müller <vinigm.nho@gmail.com>
2023-02-02 15:55:16 -0300

    Add ability to customize icon size and style

--- assets/material_icons.exs
+++ assets/material_icons.exs
@@ -5,14 +5,22 @@ defmodule MaterialIcons do
 
   use Phoenix.Component
 
-  attr :rest, :global, default: %{width: "24", height: "24", viewBox: "0 0 24 24"}
-  attr :style, :string, values: ~w(filled outlined round sharp two-tone), default: "filled"
+  attr :rest, :global
+  attr :style, :string, required: true
+  attr :size, :integer, required: true
   attr :paths, :map, required: true
   slot :inner_block, required: true
   defp svg(assigns) do
+    content = case assigns.paths[assigns.style] do 
+      nil -> raise RuntimeError, "icon does not have the following style: #{assigns.style}"
+      svg_path -> {:safe, svg_path}
+    end
+
+    assigns = assign(assigns, :content, content)
+
    ~H"""
-   <svg xmlns="http://www.w3.org/2000/svg" {@rest}>
-      <%%= {:safe, @paths[@style]} %>
+   <svg xmlns="http://www.w3.org/2000/svg" width={@size} height={@size} viewBox="0 0 24 24" {@rest}>
+      <%%= @content %>
     </svg>
    """
   end
@@ -23,6 +31,8 @@ defmodule MaterialIcons do
     Renders the `<%= func %>` icon.
     """
     attr :rest, :global, doc: "the arbitrary HTML attributes for the svg container", include: ~w(fill stroke stroke-width)
+    attr :style, :string, values: ~w(filled outlined round sharp two-tone), default: "filled"
+    attr :size, :integer, default: 24
     def <%= func %>(assigns), do: svg(assign(assigns, paths: <%= Macro.to_string(paths) %>))
   <% end %>
 end
