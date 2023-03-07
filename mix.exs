b68d50fe5b73b7a744d4b4a5f6d4733fbf2395ab
thiagomajesk <thiagomgoulart@gmail.com>
2023-03-07 10:50:30 -0300

    Package config

--- mix.exs
+++ mix.exs
@@ -1,12 +1,18 @@
 defmodule MaterialIcons.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/thiagomajesk/material_icons_elixir"
+
   def project do
     [
       app: :material_icons,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -18,12 +24,41 @@ defmodule MaterialIcons.MixProject do
     ]
   end
 
+  defp description() do
+    """
+    Material icons is a collection of icons designed by Google under the material design guidelines.
+    This library provides optimized svgs for each icon packaged as a Phoenix Component and is heavily inspired by the heroicons_elixir project.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Thiago Majesk Goulart", "Vinicius Müller"],
+      licenses: ["AGPL-3.0-only"],
+      links: %{"GitHub" => @url},
+      files: ~w(lib mix.exs README.md LICENSE)
+    ]
+  end
+
+  defp docs() do
+    [
+      source_ref: "v#{@version}",
+      main: "README",
+      canonical: "http://hexdocs.pm/material_icons",
+      source_url: @url,
+      extras: [
+        "README.md": [filename: "README"]
+      ]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:phoenix_live_view, "~> 0.18.11"},
       {:httpoison, "~> 2.0", only: [:dev, :test]},
-      {:floki, "~> 0.34.0", only: [:dev, :test]}
+      {:floki, "~> 0.34.0", only: [:dev, :test]},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
   end
 end
