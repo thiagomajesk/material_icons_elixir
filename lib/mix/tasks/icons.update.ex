97d670585d7d49e950d6d7fe7b3a1348afbdd1d4
thiagomajesk <13930952+thiagomajesk@users.noreply.github.com>
2025-03-17 09:52:16 -0300

    Properly log download errors more gracefully

--- lib/mix/tasks/icons.update.ex
+++ lib/mix/tasks/icons.update.ex
@@ -3,6 +3,7 @@ defmodule Mix.Tasks.Icons.Update do
 
   @shortdoc "Updates the icon set via download from github"
 
+  @repository "material-design-icons"
   @cdn "https://codeload.github.com"
 
   require Logger
@@ -11,20 +12,26 @@ defmodule Mix.Tasks.Icons.Update do
   def run(_args) do
     Application.ensure_all_started(:httpoison)
 
-    url = "#{@cdn}/marella/material-design-icons/zip/refs/tags/v#{Mix.Tasks.Icons.vsn()}"
-    tmp_dir = Path.join(System.tmp_dir!(), "material_icons")
+    url = "#{@cdn}/marella/#{@repository}/zip/refs/tags/v#{Mix.Tasks.Icons.vsn()}"
+    tmp_dir = Path.join(System.tmp_dir!(), @repository)
 
     ensure_dir!(tmp_dir)
 
-    with %{status_code: 200, body: contents} <- HTTPoison.get!(url),
-         {:ok, _paths} <- :zip.unzip(contents, cwd: to_charlist(tmp_dir)),
-         :ok <- copy_svg_files_from(tmp_dir) do
-      Logger.debug("Finished copying svg files to the assets directory")
+    Logger.info("Downloading the icons from #{url}")
+
+    case HTTPoison.get!(url) do
+      %{status_code: 200, body: contents} ->
+        {:ok, _paths} = :zip.unzip(contents, cwd: to_charlist(tmp_dir))
+        copy_svg_files_from!(tmp_dir)
+        Logger.debug("Finished copying svg files to the assets directory")
+
+      %{status_code: code} ->
+        Logger.error("Failed to download the icons from status code: #{code}")
     end
   end
 
-  defp copy_svg_files_from(tmp_dir) do
-    base_dir = Path.join([tmp_dir, "material-design-icons-#{Mix.Tasks.Icons.vsn()}", "svg"])
+  defp copy_svg_files_from!(tmp_dir) do
+    base_dir = Path.join([tmp_dir, "#{@repository}-#{Mix.Tasks.Icons.vsn()}", "svg"])
 
     Enum.each(Mix.Tasks.Icons.styles(), fn style ->
       dest_dir = Path.join("assets/icons", style)
